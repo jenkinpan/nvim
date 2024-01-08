d71eaecac5b7141f49e5078f5a76b67aa5f1b0d3
jenkinpan <jenkinpan0628@gmail.com>
2024-01-08 14:13:32 +0800

    Update code formatting and fix minor bugs

---
+++ init.lua
@@ -0,0 +1,19 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- 导入vim相关设置
+require("vim-options")
+
+-- 导入插件设置，文件位置在lua/plugins/*.lua
+require("lazy").setup("plugins")
+
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+
+		configs.setup({
+			auto_install = true,
+			highlight = { enalbe = true },
+			indent = { enable = true },
+		})
+	end,
+}
