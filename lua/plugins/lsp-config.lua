222e9b154fb71d3072a42d2c38e7d294e89dca72
jenkin <jenkinpan0628@gmail.com>
2024-01-08 15:07:16 +0800

    add auto compeletion into pluginins and link it to lsp for snippest.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"williamboman/mason.nvim",
+		lazy = false,
 		config = function()
 			require("mason").setup({
 				ui = {
@@ -15,19 +16,30 @@ return {
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "eslint" },
-			})
-		end,
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
 	},
 	{
 		"neovim/nvim-lspconfig",
+		lazy = false,
 		config = function()
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.tsserver.setup({})
-			lspconfig.eslint.setup({})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities
+			})
+			lspconfig.eslint.setup({
+				capabilities = capabilities
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities
+			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
