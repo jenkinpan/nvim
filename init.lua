6ac8f995df11970016243efe993fe44e36dc1133
jenkinpan <jenkinpan0628@gmail.com>
2024-01-10 15:12:47 +0800

    modify some neo-tree setting

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -16,4 +16,3 @@ require("vim-options")
 
 -- 导入插件设置，文件位置在lua/plugins/*.lua
 require("lazy").setup("plugins")
-
