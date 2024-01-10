29ba82f6fb637ec3c05488b3c1c52a623583cb68
jenkinpan <jenkinpan0628@gmail.com>
2024-01-10 23:26:17 +0800

    format file

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,3 +1,5 @@
+local map = vim.api.nvim_set_keymap
+
 vim.cmd("set number")
 vim.cmd("set autoindent")
 vim.cmd("set tabstop=4")
@@ -9,27 +11,25 @@ vim.g.mapleader = " "
 
 -- Only in neovide, the below keymaps work
 if vim.g.neovide then
-	vim.keymap.set('n', '<C-s>', ':w<CR>')   -- Save
-	vim.keymap.set('v', '<C-c>', '"+y')      -- Copy
-	vim.keymap.set('n', '<C-v>', '"+P')      -- Paste normal mode
-	vim.keymap.set('v', '<C-v>', '"+P')      -- Paste visual mode
-	vim.keymap.set('c', '<C-v>', '<C-R>+')   -- Paste command mode
-	vim.keymap.set('i', '<C-v>', '<ESC>l"+Pli') -- Paste insert mode
+	vim.keymap.set("n", "<C-s>", ":w<CR>") -- Save
+	vim.keymap.set("v", "<C-c>", '"+y') -- Copy
+	vim.keymap.set("n", "<C-v>", '"+P') -- Paste normal mode
+	vim.keymap.set("v", "<C-v>", '"+P') -- Paste visual mode
+	vim.keymap.set("c", "<C-v>", "<C-R>+") -- Paste command mode
+	vim.keymap.set("i", "<C-v>", '<ESC>l"+Pli') -- Paste insert mode
 end
 
-local map = vim.api.nvim_set_keymap 
-
 -- Allow clipboard copy paste in neovim
-map('', '<C-v>', '+p<CR>', { noremap = true, silent = true })
-map('!', '<C-v>', '<C-R>+', { noremap = true, silent = true })
-map('t', '<C-v>', '<C-R>+', { noremap = true, silent = true })
-map('v', '<C-v>', '<C-R>+', { noremap = true, silent = true })
+map("", "<C-v>", "+p<CR>", { noremap = true, silent = true })
+map("!", "<C-v>", "<C-R>+", { noremap = true, silent = true })
+map("t", "<C-v>", "<C-R>+", { noremap = true, silent = true })
+map("v", "<C-v>", "<C-R>+", { noremap = true, silent = true })
 
 -- Navigate vim panes better
-map('n', '<c-k>', ':wincmd k<CR>', { noremap = true, silent = false })
-map('n', '<c-j>', ':wincmd j<CR>', { noremap = true, silent = false })
-map('n', '<c-h>', ':wincmd h<CR>', { noremap = true, silent = false })
-map('n', '<c-l>', ':wincmd l<CR>', { noremap = true, silent = false })
+map("n", "<c-k>", ":wincmd k<CR>", { noremap = true, silent = false })
+map("n", "<c-j>", ":wincmd j<CR>", { noremap = true, silent = false })
+map("n", "<c-h>", ":wincmd h<CR>", { noremap = true, silent = false })
+map("n", "<c-l>", ":wincmd l<CR>", { noremap = true, silent = false })
 
 -- set shortcut for Neotree
 map("n", "<C-n>", ":Neotree reveal <CR>", {})
