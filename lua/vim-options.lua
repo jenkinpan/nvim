6d915e90c8c1856fe753a61c1d066e9d680b80d5
jenkinpan <jenkinpan0628@gmail.com>
2024-01-12 20:04:50 +0800

    add debug function

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -11,11 +11,11 @@ vim.g.mapleader = " "
 
 -- Only in neovide, the below keymaps work
 if vim.g.neovide then
-	vim.keymap.set("n", "<C-s>", ":w<CR>") -- Save
-	vim.keymap.set("v", "<C-c>", '"+y') -- Copy
-	vim.keymap.set("n", "<C-v>", '"+P') -- Paste normal mode
-	vim.keymap.set("v", "<C-v>", '"+P') -- Paste visual mode
-	vim.keymap.set("c", "<C-v>", "<C-R>+") -- Paste command mode
+	vim.keymap.set("n", "<C-s>", ":w<CR>")   -- Save
+	vim.keymap.set("v", "<C-c>", '"+y')      -- Copy
+	vim.keymap.set("n", "<C-v>", '"+P')      -- Paste normal mode
+	vim.keymap.set("v", "<C-v>", '"+P')      -- Paste visual mode
+	vim.keymap.set("c", "<C-v>", "<C-R>+")   -- Paste command mode
 	vim.keymap.set("i", "<C-v>", '<ESC>l"+Pli') -- Paste insert mode
 end
 
