1f0f96f6acf6056be16a63ddc8ced86253e6df8e
jenkin <jenkinpan0628@gmail.com>
2024-02-01 20:51:09 +0800

    add all ui setting in one lua file

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,69 +1,78 @@
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"rcarriga/nvim-notify",
-		"hrsh7th/nvim-cmp",
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+			"hrsh7th/nvim-cmp",
+		},
+
+		config = function()
+			require("noice").setup({
+				lsp = {
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true,
+					},
+				},
+				routes = {
+					filter = {
+						{
+							view = "notify",
+							filter = { event = "msg_showmode" },
+						},
+
+						event = "notify",
+						fidn = "No information available",
+					},
+					opts = { skip = true },
+				},
+				views = {
+					cmdline_popup = {
+						position = {
+							row = "50%",
+							col = "50%",
+						},
+						size = {
+							width = 60,
+							height = "auto",
+						},
+					},
+					popupmenu = {
+						position = {
+							row = "71%",
+							col = "50%",
+						},
+						size = {
+							width = 60,
+							height = 10,
+						},
+						border = {
+							style = "rounded",
+							padding = { 0, 1 },
+						},
+						win_options = {
+							winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+						},
+					},
+				},
+				presets = {
+					bottom_search = true,
+					command_palette = false,
+					long_message_to_split = true,
+					inc_rename = false,
+					lsp_doc_border = false,
+				},
+			})
+		end,
 	},
-
-	config = function()
-		require("noice").setup({
-			lsp = {
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true,
-				},
-			},
-			routes = {
-				filter = {
-					{
-						view = "notify",
-						filter = { event = "msg_showmode" },
-					},
-
-					event = "notify",
-					fidn = "No information available",
-				},
-				opts = { skip = true },
-			},
-			views = {
-				cmdline_popup = {
-					position = {
-						row = "50%",
-						col = "50%",
-					},
-					size = {
-						width = 60,
-						height = "auto",
-					},
-				},
-				popupmenu = {
-					position = {
-						row = "71%",
-						col = "50%",
-					},
-					size = {
-						width = 60,
-						height = 10,
-					},
-					border = {
-						style = "rounded",
-						padding = { 0, 1 },
-					},
-					win_options = {
-						winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
-					},
-				},
-			},
-			presets = {
-				bottom_search = true,
-				command_palette = false,
-				long_message_to_split = true,
-				inc_rename = false,
-				lsp_doc_border = false,
-			},
-		})
-	end,
+	{
+		"goolord/alpha-nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("alpha").setup(require("alpha.themes.startify").config)
+		end,
+	},
 }
