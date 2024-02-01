e3d54c704babff7fc5c0de2d4e241f663cb82578
jenkinpan <jenkinpan0628@gmail.com>
2024-02-01 13:50:39 +0800

    add noice require treesitter for ensure install configuration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
 		local configs = require("nvim-treesitter.configs")
 
 		configs.setup({
+			ensure_installed = { "lua", "vim", "markdown", "regex", "markdown_inline", "bash" },
 			auto_install = true,
 			highlight = { enalbe = true },
 			indent = { enable = true },
