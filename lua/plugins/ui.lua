2bff6163f19ff4bcc747fb8eab6fb875624b10a3
jenkin <jenkinpan0628@gmail.com>
2024-02-01 23:39:59 +0800

    set alpha to vimenter event

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -13,6 +13,7 @@ return {
 	{
 		-- set greeter UI
 		"goolord/alpha-nvim",
+		event = "VimEnter",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
 			require("alpha").setup(require("alpha.themes.startify").config)
