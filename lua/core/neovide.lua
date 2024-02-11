16641d778bf95322deb92af2e907be3d49d07c4d
jenkin <jenkinpan0628@gmail.com>
2024-02-11 17:11:40 +0800

    [change] change trasnparency of neovide to 0.9

--- lua/core/neovide.lua
+++ lua/core/neovide.lua
@@ -2,7 +2,7 @@ local map = vim.keymap.set
 -- Only in neovide, the below keymaps work
 -- require neovide
 if vim.g.neovide then
-	vim.g.neovide_transparency = 0.8
+	vim.g.neovide_transparency = 0.9
 	vim.g.neovide_hide_mouse_when_typing = true
 
 	map("v", "<C-c>", '"+y') -- Copy
