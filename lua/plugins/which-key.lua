d4d581e851b90231e6170bcf9dc745023f126a1e
jenkin <jenkinpan0628@gmail.com>
2024-02-25 15:01:43 +0800

    [update] update some which-key show keys shortcuts

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -5,85 +5,38 @@ return {
 		vim.o.timeout = true
 		vim.o.timeoutlen = 300
 	end,
-	opts = {
-		{
+	opts = {},
+	config = function()
+		require("which-key").setup({
 			plugins = {
-				marks = false, -- shows a list of your marks on ' and `
-				registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
 				spelling = {
-					enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-					suggestions = 20, -- how many suggestions should be shown in the list?
-				},
-				presets = {
-					operators = true, -- adds help for operators like d, y, ...
-					motions = true, -- adds help for motions
-					text_objects = true, -- help for text objects triggered after entering an operator
-					windows = true, -- default bindings on <c-w>
-					nav = false, -- misc bindings to work with windows
-					z = false, -- bindings for folds, spelling and others prefixed with z
-					g = false, -- bindings for prefixed with g
+					enabled = true,
+					suggestions = 20,
 				},
 			},
-			operators = { gcc = "Comments" },
-			key_labels = {
-				-- For example:
-				-- ["<space>"] = "SPC",
-				-- ["<cr>"] = "RET",
-				-- ["<tab>"] = "TAB",
+		})
+
+		require("which-key").register({
+			["<leader>"] = {
+				f = {
+					name = "Telescope",
+					f = { "<cmd>Telescope find_files<cr>", "Find File" },
+					g = { "<cmd>Telescope live_grep<cr>", "Grep" },
+					b = { "<cmd>Telescope buffers<cr>", "Buffers" },
+					h = { "<cmd>Telescope help_tags<cr>", "Help" },
+				},
+				d = {
+					name = "Debug",
+					t = { "<cmd>lua require('dap').toggle_breakpoint()<cr>", "Toggle Breakpoint" },
+					c = { "<cmd>lua require('dap').continue()<cr>", "Continue" },
+				},
+				b = {
+					name = "Buffers",
+					p = { "<cmd>BufferLinePick<cr>", "Pick buffer" },
+					c = { "<cmd>BufferLinePickClose<cr>", "Close pick" },
+					o = { "<cmd>BufferLineCloseOthers<cr>", "Close others" },
+				},
 			},
-			motions = {
-				count = true,
-			},
-			icons = {
-				breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-				separator = "➜", -- symbol used between a key and it's label
-				group = "+", -- symbol prepended to a group
-			},
-			popup_mappings = {
-				scroll_down = "<c-d>", -- binding to scroll down inside the popup
-				scroll_up = "<c-u>", -- binding to scroll up inside the popup
-			},
-			window = {
-				border = "none", -- none, single, double, shadow
-				position = "bottom", -- bottom, top
-				margin = { 1, 0, 1, 0 },
-				padding = { 1, 2, 1, 2 },
-				winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
-				zindex = 1000,
-			},
-			layout = {
-				height = { min = 4, max = 25 },
-				width = { min = 20, max = 50 },
-				spacing = 3,
-				align = "left",
-			},
-			ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
-			hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "^:", "^ ", "^call ", "^lua " }, -- hide mapping boilerplate
-			show_help = true, -- show a help message in the command line for using WhichKey
-			show_keys = true, -- show the currently pressed key and its label as a message in the command line
-			triggers = "auto", -- automatically setup triggers
-			-- triggers = {"<leader>"} -- or specifiy a list manually
-			-- list of triggers, where WhichKey should not wait for timeoutlen and show immediately
-			triggers_nowait = {
-				-- marks
-				"`",
-				"'",
-				"g`",
-				"g'",
-				-- registers
-				'"',
-				"<c-r>",
-				-- spelling
-				"z=",
-			},
-			triggers_blacklist = {
-				i = { "j", "k" },
-				v = { "j", "k" },
-			},
-			disable = {
-				buftypes = {},
-				filetypes = {},
-			},
-		},
-	},
+		})
+	end,
 }
