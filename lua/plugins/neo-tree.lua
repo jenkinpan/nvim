3ccc0019f3f108bd4fde3737df3ac31290bd3b46
jenkin <jenkinpan0628@gmail.com>
2024-02-26 18:49:06 +0800

    [update] update neo-tree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -38,10 +38,10 @@ return {
 			popup_border_style = "rounded",
 			enable_git_status = true,
 			enable_diagnostics = true,
-			enable_normal_mode_for_inputs = false,                    -- Enable normal mode for input dialogs.
+			enable_normal_mode_for_inputs = false, -- Enable normal mode for input dialogs.
 			open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
-			sort_case_insensitive = false,                            -- used when sorting files and directories in the tree
-			sort_function = nil,                                      -- use a custom function for sorting files and directories in the tree
+			sort_case_insensitive = false, -- used when sorting files and directories in the tree
+			sort_function = nil, -- use a custom function for sorting files and directories in the tree
 			-- sort_function = function (a,b)
 			--       if a.type == b.type then
 			--           return a.path > b.path
@@ -88,10 +88,10 @@ return {
 				git_status = {
 					symbols = {
 						-- Change type
-						added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
-						modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
-						deleted = "✖", -- this can only be used in the git_status source
-						renamed = "󰁕", -- this can only be used in the git_status source
+						added = "✚",
+						modified = "",
+						deleted = "✖",
+						renamed = "󰁕",
 						-- Status type
 						untracked = "",
 						ignored = "",
@@ -212,11 +212,11 @@ return {
 					},
 				},
 				follow_current_file = {
-					enabled = false,        -- This will find and focus the file in the active buffer every time
+					enabled = false, -- This will find and focus the file in the active buffer every time
 					--               -- the current file is changed while the tree is open.
 					leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
 				},
-				group_empty_dirs = false,   -- when true, empty folders will be grouped together
+				group_empty_dirs = false, -- when true, empty folders will be grouped together
 				hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
 				-- in whatever position is specified in window.position
 				-- "open_current",  -- netrw disabled, opening a directory opens within the
