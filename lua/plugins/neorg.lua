e6ec5e2a7d5fffd513139916f1bbd54cc149729e
jenkin <jenkinpan0628@gmail.com>
2024-03-25 11:20:22 +0800

    update neorg config

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,47 +1,52 @@
 return {
-	"nvim-neorg/neorg",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	build = ":Neorg sync-parsers",
-	-- tag = "*",
-	lazy = true, -- enable lazy load
-	ft = "norg", -- lazy load on file type
-	cmd = "Neorg", -- lazy load on command
-	config = function()
-		require("neorg").setup({
-			load = {
-				["core.defaults"] = {}, -- Loads default behaviour
-				["core.concealer"] = {}, -- Adds pretty icons to your documents
-				["core.summary"] = {},
-				["core.ui"] = {},
-				["core.export"] = {
-					config = {
-						export_dir = "/home/jenkin/Documents/markdown-export",
-					},
-				},
-				["core.dirman"] = { -- Manages Neorg workspaces
-					config = {
-						workspaces = {
-							notes = "~/Documents/notes",
+	{
+		"vhyrro/luarocks.nvim",
+		priority = 1000,
+		config = true,
+	},
+	{
+		"nvim-neorg/neorg",
+		dependencies = { "luarocks.nvim" },
+		lazy = true, -- enable lazy load
+		ft = "norg", -- lazy load on file type
+		cmd = "Neorg", -- lazy load on command
+		config = function()
+			require("neorg").setup({
+				load = {
+					["core.defaults"] = {}, -- Loads default behaviour
+					["core.concealer"] = {}, -- Adds pretty icons to your documents
+					["core.summary"] = {},
+					["core.ui"] = {},
+					["core.export"] = {
+						config = {
+							export_dir = "/home/jenkin/Documents/markdown-export",
 						},
-						index = "index.norg",
+					},
+					["core.dirman"] = { -- Manages Neorg workspaces
+						config = {
+							workspaces = {
+								notes = "~/Documents/notes",
+							},
+							index = "index.norg",
+						},
+					},
+					["core.completion"] = {
+						config = {
+							engine = "nvim-cmp",
+						},
+					},
+					["core.qol.toc"] = {
+						config = {
+							close_after_use = true,
+						},
+					},
+					["core.presenter"] = {
+						config = {
+							zen_mode = "zen-mode",
+						},
 					},
 				},
-				["core.completion"] = {
-					config = {
-						engine = "nvim-cmp",
-					},
-				},
-				["core.qol.toc"] = {
-					config = {
-						close_after_use = true,
-					},
-				},
-				["core.presenter"] = {
-					config = {
-						zen_mode = "zen-mode",
-					},
-				},
-			},
-		})
-	end,
+			})
+		end,
+	},
 }
