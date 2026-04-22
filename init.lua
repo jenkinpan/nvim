333a8d78f58a309e05aeb74ec4dbed4d5a32c13c
jenkin <jenkinpan0628@gmail.com>
2026-04-22 20:05:31 +0800

    update

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
-if vim.loader then vim.loader.enable() end
+if vim.loader then
+	vim.loader.enable()
+end
 
-vim.g.mapleader = " "
-
-require('core.option')
-require('core.pack')
-
+require("core.option")
+require("core.keymaps")
+require("core.pack")
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,33 +1,29 @@
 require("auto-dark-mode").setup({
-	opts = {
-		update_interval = 1000,
-		set_dark_mode = function()
-			vim.api.nvim_set_option_value("background", "dark", {})
-			vim.cmd("colorscheme tokyonight")
-		end,
-		set_light_mode = function()
-			vim.api.nvim_set_option_value("background", "light", {})
-			vim.cmd("colorscheme tokyonight")
-		end,
-	},
+	update_interval = 1000,
+	set_dark_mode = function()
+		vim.api.nvim_set_option_value("background", "dark", {})
+		vim.cmd("colorscheme tokyonight")
+	end,
+	set_light_mode = function()
+		vim.api.nvim_set_option_value("background", "light", {})
+		vim.cmd("colorscheme tokyonight")
+	end,
 })
 
 require("tokyonight").setup({
-	opts = {
-		transparent = true,
-		styles = {
-			sidebars = "transparent",
-			floats = "transparent",
-		},
-		-- set lualine and snake.explore transparent
-		on_colors = function(colors)
-			colors.bg_statusline = colors.none
-		end,
-		on_highlights = function(hl, c)
-			-- TabLineFill is currently set to black
-			hl.TabLineFill = {
-				bg = c.none,
-			}
-		end,
+	transparent = true,
+	styles = {
+		sidebars = "transparent",
+		floats = "transparent",
 	},
+	-- set lualine and snake.explore transparent
+	on_colors = function(colors)
+		colors.bg_statusline = colors.none
+	end,
+	on_highlights = function(hl, c)
+		-- TabLineFill is currently set to black
+		hl.TabLineFill = {
+			bg = c.none,
+		}
+	end,
 })
